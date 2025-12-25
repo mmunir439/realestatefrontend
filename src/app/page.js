a4472a6cf9e5b4cfda7acbbaf9d4021d9d1c6b83
Muhammad Munir <hrcoder3@gmail.com>
2025-12-25 19:36:11 +0500

    this is added in here the visiion in here

--- src/app/page.js
+++ src/app/page.js
@@ -9,18 +9,18 @@ import Projects from '@/components/Projects';
 import Process  from '@/components/Process';
 import Welcome from '@/components/Welcome';
 export default function Home() {
-  async function getUser() {
-    try {
-      const response = await axios.get("http://localhost:5000/");
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  // async function Munir() {
+  //   try {
+  //     const response = await axios.get("http://localhost:5000/");
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
-  useEffect(() => {
-    getUser();
-  }, []);
+  // useEffect(() => {
+  //   Munir();
+  // }, []);
 
   return (
     <>
